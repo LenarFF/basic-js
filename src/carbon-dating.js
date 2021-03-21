82de640dd4e40a322d2fbe0c04f1b7e44f754b0f
Lenar <flfforwork@gmail.com>
2021-03-21 11:04:02 +0300

    feat: add solve 3 first task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
-const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+  
   // remove line with error and write your code here
+  if (typeof sampleActivity !== "string" ||
+    Number.isNaN(+sampleActivity) ||
+    sampleActivity <= 0 ||
+    sampleActivity > MODERN_ACTIVITY
+      ) return false
+let k = 0.693 / HALF_LIFE_PERIOD;
+let t = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
+return Math.ceil(t)
 };
+ 
+
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) return false
+  let dtName = [];
+  for (let member of members) {
+    if (typeof member === "string") {
+      dtName.push(member.trim()[0].toUpperCase());
+    }
+  }
+  return dtName.sort().join('')
+}
