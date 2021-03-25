f8433c8878a051943c88d817367e4b08645b9f3e
Lenar <flfforwork@gmail.com>
2021-03-25 23:53:46 +0300

    feat: add solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,25 @@
-const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let sep = options.separator ? options.separator : '+';
+  let addSep = options.additionSeparator ? options.additionSeparator : '|';
+  let times = options.repeatTimes ? options.repeatTimes : 1;
+  let addTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+  let result = '';
+  str = String(str);
+  for (let i = 0; i < times; i++) {
+    result += str;
+    if (options.addition !== undefined) {
+    for (let j = 0; j < addTimes; j++) {
+      result += options.addition;
+      if (j < addTimes - 1) {
+        result += addSep;
+      }
+    } 
+  }
+    if (i < times - 1) {
+    result += sep;
+    }
+  }
+  return result
 };
   
