5f7f9ca7f0278564c05b1e152f574584267ca198
Lenar <flfforwork@gmail.com>
2021-03-26 12:26:34 +0300

    feat: add right solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -5,42 +5,49 @@ class VigenereCipheringMachine {
     this.letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   }
   encrypt(message, key) {
+    if (!message || !key) throw new Error('ERROR');
     message = message.toLowerCase();
     let result = [];
     let newKey = '';
-    while(key.length <= message.length) {
+    
+    while(newKey.length <= message.length) {
       newKey += key.toLowerCase();
     }
-    if (!message || !key) throw new Error('ERROR');
+    let keyInd = 0; 
     for (let i = 0; i < message.length; i++) {
       if (this.letters.includes(message[i]) ) {
         result.push(this.letters[(this.letters.indexOf(message[i]) +
-         this.letters.indexOf(newKey[i])) % this.letters.length]);
+         this.letters.indexOf(newKey[keyInd])) % this.letters.length]);
+         keyInd++
       } else {
         result.push(message[i]);
       }
+      
     }
-    return this.direction === false ?  result.reverse.join('').toUpperCase() : result.join('').toUpperCase();
+    return this.direction === false ?  result.reverse().join('').toUpperCase() : result.join('').toUpperCase();
     }
  
 
   decrypt(message, key) {
-    message = message.toLowerCase();
     if (!message || !key) throw new Error('ERROR');
+    message = message.toLowerCase();
+    
     let result = [];
     let newKey = '';
-    while(key.length <= message.length) {
+    while(newKey.length <= message.length) {
       newKey += key.toLowerCase();
     }
+    let keyInd = 0;
     for (let i = 0; i < message.length; i++) {
       if (this.letters.includes(message[i]) ) {
         result.push(this.letters[(this.letters.indexOf(message[i]) -
-        this.letters.indexOf(newKey[i]) + this.letters.length) % this.letters.length ] )
+        this.letters.indexOf(newKey[keyInd]) + this.letters.length) % this.letters.length ] );
+        keyInd++
       } else {
         result.push(message[i]);
       }
     }
-    return this.direction === false ?  result.reverse.join('').toUpperCase() : result.join('').toUpperCase();
+    return this.direction === false ?  result.reverse().join('').toUpperCase() : result.join('').toUpperCase();
   }
 }
 
